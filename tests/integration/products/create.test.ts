7da493cd5b0f2d1a5e78fb7554a3315d9f2e7d64
Matheus Pozett <maa_souza11@hotmail.com>
2026-01-13 11:42:03 -0300

    test: cria teste de integração para rota POST /products

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -1,10 +1,20 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app';
+import productsMocks from '../../mocks/products.mocks';
+import ProductModel from '../../../src/database/models/product.model';
 
 chai.use(chaiHttp);
 
 describe('POST /products', function () { 
   beforeEach(function () { sinon.restore(); });
-
+  it('Retorna status 201 CREATED e o objeto com novo produto', async function() {
+    const productInstance = ProductModel.build(productsMocks.successCreated);
+    sinon.stub(ProductModel, 'create').resolves(productInstance);
+    const response = await chai.request(app).post('/products').send(productsMocks.successBody)
+    console.log(productInstance)
+    expect(response.status).to.be.eq(201);
+    expect(response.body).to.be.deep.eq(productsMocks.successCreated)
+  });
 });
---
+++ tests/mocks/products.mocks.ts
@@ -0,0 +1,14 @@
+const successBody = {
+  name: "Martelo de Thor",
+  price: "30 peças de ouro",
+  userId: 1
+};
+
+const successCreated = {
+  id: 6,
+  name: "Martelo de Thor",
+  price: "30 peças de ouro",
+  userId: 1
+}
+
+export default {successBody, successCreated};
