60013cb4662d6d9ebe48ef9556bc4c4513ab03c2
Matheus Pozett <maa_souza11@hotmail.com>
2026-01-13 11:40:26 -0300

    test: cria teste unitario para função create do service

--- tests/unit/services/products.service.test.ts
+++ tests/unit/services/products.service.test.ts
@@ -1,7 +1,18 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import productsMocks from '../../mocks/products.mocks';
+import ProductModel from '../../../src/database/models/product.model';
+import productsService from '../../../src/services/products.service';
 
 describe('ProductsService', function () {
   beforeEach(function () { sinon.restore(); });
 
+  it('Cria produto no banco de dados com sucesso', async function() {
+    const productInstance = ProductModel.build(productsMocks.successCreated);
+    sinon.stub(ProductModel, 'create').resolves(productInstance);
+
+    const result = await productsService.create(productsMocks.successBody);
+
+    expect(result).to.have.deep.eq(productInstance.dataValues);
+  });
 });
