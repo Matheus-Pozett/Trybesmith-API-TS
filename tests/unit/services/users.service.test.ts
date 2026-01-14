481c5bf5517dc435c7d136df263ad222822e0693
Matheus Pozett <maa_souza11@hotmail.com>
2026-01-14 07:47:46 -0300

    test: Testa função listAll da camada de serviço

--- tests/unit/services/users.service.test.ts
+++ tests/unit/services/users.service.test.ts
@@ -1,7 +1,34 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import UserModel from '../../../src/database/models/user.model';
+import userService from '../../../src/services/user.service';
+import userMocks from '../../mocks/user.mocks';
 
 describe('UsersService', function () {
   beforeEach(function () { sinon.restore(); });
 
+  it('Função listAll retorna lista de todos os usuarios', async function() {
+    const dbData = [
+      {
+        username: 'Hagar',
+        productIds: [ { id: 1 }, { id: 2 } ]
+      },
+      {
+        username: 'Eddie',
+        productIds: [ { id: 3 }, { id: 4 } ]
+      }
+    ];
+
+    const usersInstances = dbData.map((u) => {
+      const user = UserModel.build(u as any);
+      (user as any).dataValues.productIds = u.productIds;
+      return user;
+    });
+
+    sinon.stub(UserModel, 'findAll').resolves(usersInstances);
+
+    const result = await userService.listAll();
+
+    expect(result).to.be.deep.eq(userMocks.allUsersMocks);
+  });
 });
