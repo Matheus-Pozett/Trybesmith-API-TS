3440b8610c662b4df3f62a50d1863a33049204d6
Matheus Pozett <maa_souza11@hotmail.com>
2026-01-14 07:46:56 -0300

    feat: cria rota GET /users

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import ProductsRouter from './routes/products.routes';
+import UserRouter from './routes/user.routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(ProductsRouter);
+app.use(UserRouter);
 
 export default app;
