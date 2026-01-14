d2bb428b8e625afc1f9f9b5fc1d0981c5670e58b
Matheus Pozett <maa_souza11@hotmail.com>
2026-01-14 07:47:16 -0300

    test: teste de integração da rota get /users

--- tests/integration/users/list.test.ts
+++ tests/integration/users/list.test.ts
@@ -1,10 +1,38 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import UserModel from '../../../src/database/models/user.model';
+import app from '../../../src/app';
+import userMocks from '../../mocks/user.mocks';
 
 chai.use(chaiHttp);
 
 describe('GET /users', function () { 
   beforeEach(function () { sinon.restore(); });
 
+  it('Retorna status 200 e uma lista com username e productsId', async function() {
+    const dbData = [
+      {
+        username: 'Hagar',
+        productIds: [ { id: 1 }, { id: 2 } ]
+      },
+      {
+        username: 'Eddie',
+        productIds: [ { id: 3 }, { id: 4 } ]
+      }
+    ];
+
+    const usersInstances = dbData.map((u) => {
+      const user = UserModel.build(u as any);
+      (user as any).dataValues.productIds = u.productIds;
+      return user;
+    });
+
+    sinon.stub(UserModel, 'findAll').resolves(usersInstances);
+
+    const response = await chai.request(app).get('/users');
+
+    expect(response.status).to.be.eq(200);
+    expect(response.body).to.be.deep.eq(userMocks.allUsersMocks);
+  });
 });
---
+++ tests/mocks/user.mocks.ts
@@ -0,0 +1,18 @@
+const allUsersMocks = [
+	{
+		username: "Hagar",
+		productIds: [
+			1,
+			2
+		]
+	},
+	{
+		username: "Eddie",
+		productIds: [
+			3,
+			4
+		]
+	},
+]
+
+export default {allUsersMocks}
