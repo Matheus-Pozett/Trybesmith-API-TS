ef6d19f6808a6aab68f2de0bc3896c69f2246fee
Matheus Pozett <maa_souza11@hotmail.com>
2026-01-14 07:47:59 -0300

    feat: testa camada controller, função listAll

--- tests/unit/controllers/users.controller.test.ts
+++ tests/unit/controllers/users.controller.test.ts
@@ -2,6 +2,9 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
+import userService from '../../../src/services/user.service';
+import userMocks from '../../mocks/user.mocks';
+import userController from '../../../src/controllers/user.controller';
 
 chai.use(sinonChai);
 
@@ -15,4 +18,12 @@ describe('UsersController', function () {
     sinon.restore();
   });
 
+  it('Controller retorna status 200 e lista de usuarios', async function() {
+    sinon.stub(userService, 'listAll').resolves(userMocks.allUsersMocks);
+
+    await userController.listAll(req ,res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(userMocks.allUsersMocks);
+  });
 });
