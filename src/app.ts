1a28301bc698373ab90cbc228f77d7164b82254a
Matheus Pozett <maa_souza11@hotmail.com>
2026-01-29 09:07:29 -0300

    refactor: refatora app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
+import 'express-async-errors';
 import express from 'express';
 import ProductsRouter from './routes/products.routes';
 import UserRouter from './routes/user.routes';
+import loginRouter from './routes/login.routes';
+import globalError from './middleware/globalErrors.middleware';
 
 const app = express();
 
 app.use(express.json());
 
-app.use(ProductsRouter);
-app.use(UserRouter);
+app.use('/login', loginRouter);
+app.use('/products', ProductsRouter);
+app.use('/users', UserRouter);
+app.use(globalError);
 
 export default app;
